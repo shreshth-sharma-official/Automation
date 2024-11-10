d39864d0702c98531b97ed531aed785a130e7872
Shreshth Sharma official <61823543+shreshth-sharma-official@users.noreply.github.com>
2024-11-10 18:49:16 +0530

    add throw deceleration

--- src/test/java/automation/Demo.java
+++ src/test/java/automation/Demo.java
@@ -10,7 +10,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Demo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver=new ChromeDriver();
@@ -24,7 +24,7 @@ public class Demo {
 //		Thread.sleep(2000);
 		dd.click();
 		
-//		Thread.sleep(5000);
+		Thread.sleep(5000);
 	    driver.close();
 	}
 
